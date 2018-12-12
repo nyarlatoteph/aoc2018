807ac4dc6fcba3d6262dd48447a0429e48f49b4c
Rudi Alberda <rudi.alberda@knowlogy.nl>
2018-12-12 21:04:44 +0100

    Day 7 part 2

--- day7.py
+++ day7.py
@@ -27,32 +27,83 @@ for m in p.matchers:
 print steps
 print prereqs
 
-def can_do_step(n):
-    return n not in prereqs or all([c in done for c in prereqs[n]])
 
+def pick_step(todo, done):
+    def can_do_step(step):
+        return step not in prereqs or all([c in done for c in prereqs[step]])
 
-done = ''
-todo = froms
-
-while len(todo) > 0:
-    todo = sorted(set(todo))
-    print done, "".join(todo)
     n = 0
     while n < len(todo) and not can_do_step(todo[n]):
         n += 1
 
-    now = todo[n]
-    del todo[n]
-    done += now
+    if n < len(todo):
+        next = todo[n]
+        del todo[n]
+        return next
+    else:
+        return None
 
-    if now not in steps:
+
+def next_step(steps):
+    next = []
+    for i in steps[step]:
+        if i not in done:
+            next.append(i)
+    return next
+
+
+done = ''
+todo = sorted(froms)
+
+while len(todo) > 0:
+    step = pick_step(todo, done)
+    done += step
+
+    if step not in steps:
         break # No more steps available
 
-    for i in steps[now]:
-        if i not in done:
-            todo.append(i)
+    todo = sorted(set(todo + next_step(steps)))
 
 
 print done
 
-workers = [0, 0, 0, 0, 0]
+workers = []
+max_workers = 5
+time_offset = 61
+
+
+def assign_worker(workers, step):
+    return workers + [(step, time_offset + ord(step) - ord('A'))]
+
+
+def work(workers):
+    (step, time) = min(workers, key = lambda w: w[1])
+    return ([(w[0], w[1] - time) for w in workers], step, time)
+
+
+done = ''
+todo = sorted(froms)
+total_time = 0
+
+while len(todo) > 0 or len(workers) > 0:
+    while len(todo) > 0 and len(workers) < max_workers:
+        step = pick_step(todo, done)
+        if step is not None and step not in done and step not in [w[0] for w in workers]:
+            workers = assign_worker(workers, step)
+        else:
+            break
+
+    print total_time, workers, "".join(todo), done
+
+    (workers, step, time) = work(workers)
+    workers = [w for w in workers if w[1] > 0]
+    total_time += time
+    done += step
+
+    if step not in steps:
+        break # No more steps available
+
+    todo = sorted(set(todo + next_step(steps)))
+
+print done
+print total_time
